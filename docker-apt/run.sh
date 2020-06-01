3c10762b2bff2f40f28208f586cf7f93f5efec0b
aki <mark@aur.ll.to>
2020-06-02 01:01:31 +0900

    .

--- docker-apt/run.sh
+++ docker-apt/run.sh
@@ -3,6 +3,4 @@ set -eux
 
 chown apt-cacher-ng:apt-cacher-ng /var/cache/apt-cacher-ng /var/log/apt-cacher-ng
 
-/etc/init.d/apt-cacher-ng start
-
-tail -f /var/log/apt-cacher-ng/*
+/usr/sbin/apt-cacher-ng -c /etc/apt-cacher-ng
